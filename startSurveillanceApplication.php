f823da3130662d0208ae7de5cb19c0e5b31e89c7
jktubs <jenskramer@gmx.com>
2015-11-02 17:29:50 +0100

    changed path

--- startSurveillanceApplication.php
+++ startSurveillanceApplication.php
@@ -1,7 +1,7 @@
 <?php
 echo "Starting Surveillance Application";
 
-$command = escapeshellcmd('sudo -u www-data python /home/pi/background_substraction/image_observation_refactored.py');
+$command = escapeshellcmd('sudo -u www-data python /var/www/image_observation_refactored.py');
 $output = shell_exec($command);
 #echo nl2br($output);
 ?>
