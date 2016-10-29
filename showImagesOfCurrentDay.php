7aa46ce05c7cc13e584c80997f20166472a859ad
jktubs <jenskramer@gmx.com>
2016-10-29 13:15:44 +0200

    replaced fixed IP

--- showImagesOfCurrentDay.php
+++ showImagesOfCurrentDay.php
@@ -1,8 +1,10 @@
 
 
 <?php
-
-$currentDay = "2016-02-02";
+$serverIP = $_SERVER['SERVER_ADDR'];
+$currentDay = "2016-10-27";
+echo nl2br("Today: ".$currentDay."\n");
+echo nl2br("\nServer:".$serverIP);
 $dirPathRoot = "images"; 
 $files = array();
 $dir = new DirectoryIterator("/var/www/".$dirPathRoot."/".$currentDay."/");
@@ -17,6 +19,6 @@ ksort($files);
 foreach($files as $file)
 {
    echo nl2br ("\n".$file."\n");
-   echo "<img src=\"http://192.168.178.30/".$dirPathRoot."/".$currentDay."/".$file."\" align=middle>";
+   echo "<img src=\"http://".$serverIP."/".$dirPathRoot."/".$currentDay."/".$file."\" align=middle>";
 }
 ?>
