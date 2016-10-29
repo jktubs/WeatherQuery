88de97d273a411bcb7c953f7f8dd88b2e4e3bcd8
jktubs <jenskramer@gmx.com>
2016-10-29 13:14:56 +0200

    replaced fixed IP

--- showCurrentImage.php
+++ showCurrentImage.php
@@ -1,3 +1,7 @@
 <?php
-echo "<img src=\"http://192.168.178.30/images/currentImage/currentImage.jpg\" align=middle>";
+$serverIP = $_SERVER['SERVER_ADDR'];
+echo nl2br("Server: ".$serverIP."\n\n");
+$now = date('l jS \of F Y h:i:s A');
+echo nl2br("Now: ".$now."\n\n");
+echo "<img src=\"http://".$serverIP."/images/currentImage/currentImage.jpg\" align=middle>";
 ?>
